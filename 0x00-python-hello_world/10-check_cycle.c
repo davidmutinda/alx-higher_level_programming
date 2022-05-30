9515ceadf8da85af613d65fa52b67c4121864c5c
davidmutinda <davidkymutinda@gmail.com>
2022-05-30 18:14:25 +0000

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,13 +8,15 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *head = list;
+	listint_t *prev = list;
 
 	if (list == NULL)
 		return (0);
 	while (list->next != NULL)
 	{
-		if (list->next == head)
+		if (list->next == head || list->next == prev)
 			return (1);
+		prev = list;
 		list = list->next;
 	}
 	return (0);
