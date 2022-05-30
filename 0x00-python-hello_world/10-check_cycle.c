69dbdbb795eb0534f009d144979cd4f691438883
davidmutinda <davidkymutinda@gmail.com>
2022-05-30 14:29:07 +0000

    Create 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: pointer to linked list
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head;
+
+	head = list;
+
+	while(1)
+	{
+		if (list->next == head)
+			return (1);
+		if (list->next == NULL)
+			return(0);
+		list = list->next;
+	}
+}
