c69eb839f810330ece3b463b62bb867f5f6575e6
davidmutinda <davidkymutinda@gmail.com>
2022-05-31 12:29:39 +0000

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for i in range(0, 89):
-    if i < 10:
-        i = '0' + str(i)
-    elif i / 10 >= i % 10:
+    if i / 10 >= i % 10:
         i += 1
         continue
+    if i < 10:
+        i = '0' + str(i)
     print("{}".format(i), end=", ")
     i = int(i)
     i += 1
