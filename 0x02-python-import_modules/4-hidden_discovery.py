ffb5e8e0c7a1dbb65f868162069585fe03a25da0
devmutinda <davidkymutinda@gmail.com>
2022-06-02 19:43:36 +0000

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,8 @@ import hidden_4
 if __name__ == "__main__":
     i = 0
     new = dir(hidden_4)
-    while i < len(new)):
+
+    while i < len(new):
         if new[i][:2] != "__":
             print("{}".format(new))
         i += 1
