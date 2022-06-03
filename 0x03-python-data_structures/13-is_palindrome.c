edf0635de57158bd01554109969da9e384e8439d
devmutinda <davidkymutinda@gmail.com>
2022-06-03 14:53:32 +0000

    Create 13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks if linked list is palindrome
+ * @head: pointer to head of linked list
+ * Return: 0 or 1
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *ptr = *head;
+	listint_t *rev;
+	listint_t *hld;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	rev = malloc(sizeof(listint_t));
+	hld = rev;
+	rev->n = ptr->n;
+	rev = malloc(sizeof(listint_t));
+	hld->next = NULL;
+	ptr = ptr->next;
+	while (ptr != NULL)
+	{
+		rev->n = ptr->n;
+		rev->next = hld;
+		hld = rev;
+		rev = malloc(sizeof(listint_t));
+		ptr = ptr->next;
+	}
+	free(rev);
+	ptr = *head;
+	rev = hld;
+
+	while (ptr != NULL)
+	{
+		if (rev->n != ptr->n)
+			return (0);
+		ptr = ptr->next;
+		rev = rev->next;
+	}
+	return (1);
+}
