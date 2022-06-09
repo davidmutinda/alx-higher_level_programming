9c65d64e28cf17f08a3695419b002daabd8d679a
devmutinda <davidkymutinda@gmail.com>
2022-06-09 05:19:36 +0000

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return
+    if not set_1:
+        return set_1
+    elif not set_2:
+        return set_2
     common = set()
     for num in set_1:
         for com in set_2:
