c4ba2861240b44aa9a02de891122b1dda1d22b13
devmutinda <davidkymutinda@gmail.com>
2022-06-09 05:24:52 +0000

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if not set_1:
+    if not set_1 and not set_2:
         return set_1
-    elif not set_2:
+    elif not set_1:
         return set_2
+    elif not set_2:
+        return set_1
     notCommon = set()
     for char in set_1:
         i = 0
