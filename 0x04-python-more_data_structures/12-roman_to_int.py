d20e42a1eadf515494becf112fd1990f5e6399aa
devmutinda <davidkymutinda@gmail.com>
2022-06-09 06:22:51 +0000

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) != str or not roman_string:
+        return 0
+    integer = 0
+    prev = 3999
+    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    for char in roman_string:
+        for key, value in dictionary.items():
+            if char is key:
+                if prev < value:
+                    integer += value - (2 * prev)
+                else:
+                    integer += value
+                prev = value
+                break
+
+    return integer
