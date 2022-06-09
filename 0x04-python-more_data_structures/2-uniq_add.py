ae2ae5e7b3e145bc16295c195318e92d1b0bac8a
devmutinda <davidkymutinda@gmail.com>
2022-06-09 05:15:50 +0000

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if not my_list:
-        return
     i, result = 0, 0
+    if not my_list:
+        return result
     for num in sorted(my_list):
         if num is sorted(my_list)[i - 1] and i > 0:
             i += 1
