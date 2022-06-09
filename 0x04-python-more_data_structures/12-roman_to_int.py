9a743e4b640a171efffd607106f11d1955776d5e
devmutinda <davidkymutinda@gmail.com>
2022-06-09 06:24:36 +0000

    Updatewq 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if type(roman_string) != str or not roman_string:
         return 0
     integer = 0
-    prev = 3999
+    prev = 1000
     dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
     for char in roman_string:
