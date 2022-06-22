cc69944c57320c128e9892dbe8c02d0a72d4c38f
devmutinda <davidkymutinda@gmail.com>
2022-06-22 15:27:54 +0000

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,7 +6,5 @@ This module defines a Square class
 
 class Square:
     """An empty class that defines a square
-    Attributes:
-        __dict__ (dict): An empty dictionary
     """
-    __dict__ = {}
+    pass
