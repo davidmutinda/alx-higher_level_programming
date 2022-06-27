e3a43111e7b111d22fff215ad4a5b5ef16bdd177
devmutinda <davidkymutinda@gmail.com>
2022-06-27 08:47:11 +0000

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -25,8 +25,15 @@ def text_indentation(text):
     while(i < len(text)):
         if text[i] in (".", "?", ":"):
             print("{}\n".format(text[i]))
-            while (text[i + 1] == ' '):
+            i += 1
+            if i == len(text):
+                break
+            while (text[i] == ' '):
                 i += 1
-        else:
-            print(text[i], end="")
+                if i == len(text):
+                    break
+
+            if i == len(text):
+                break
+        print(text[i], end="")
         i += 1
