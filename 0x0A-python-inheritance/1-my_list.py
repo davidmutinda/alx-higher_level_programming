b2a28a7c550b4a41aef4a24793b7d9ba266874a8
devmutinda <davidkymutinda@gmail.com>
2022-07-04 11:23:50 +0000

    Create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains MyList which inherits from list
+"""
+
+
+class MyList(list):
+    """
+    This class inherits from MyList
+    """
+    def __init__(self):
+        pass
+
+    def print_sorted(self):
+        print(sorted(self))
