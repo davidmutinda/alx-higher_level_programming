81ebab89dc994b0bb4b08820009944362689890f
devmutinda <davidkymutinda@gmail.com>
2022-07-05 17:26:34 +0000

    Create 100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains append_after function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    inserts a line to a file, after each line containing a specific string
+
+    Args:
+        filename(str) : the name of the file
+        search_string(str) : the string you're searching for
+        new_string(str) : the string to append to file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        contents = f.readlines()
+
+        for i in range(len(contents)):
+            if search_string in contents[i]:
+                contents.insert(i + 1, new_string)
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        contents = "".join(contents)
+        f.write(contents)
