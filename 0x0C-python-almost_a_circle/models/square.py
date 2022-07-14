099d54cad5d9d041072c65991f810b09d9b2e807
devmutinda <davidkymutinda@gmail.com>
2022-07-14 18:43:38 +0000

    Added update method to models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,8 +26,8 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """Assigns attributes"""
