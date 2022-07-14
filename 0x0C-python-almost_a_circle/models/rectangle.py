bbb8116f0eadadd11167070345cf6271136af3a5
devmutinda <davidkymutinda@gmail.com>
2022-07-14 18:54:57 +0000

    Added to_dictionary method to models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -129,3 +129,13 @@ class Rectangle(Base):
                 elif i == 4:
                     self.y = arg
                 i += 1
+
+    def to_dictionary(self):
+        """returns dictionary representation"""
+        return {
+                'x': self.x,
+                'y': self.y,
+                'id': self.id,
+                'height': self.height,
+                'width': self.width
+                }
