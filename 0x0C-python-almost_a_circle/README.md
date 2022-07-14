7656477564c69788faa58c356cd78f0492da8a74
devmutinda <davidkymutinda@gmail.com>
2022-07-14 15:02:32 +0000

    Create models/base.py

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+A review about everything on python
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+"""
+This module contains class Base
+"""
+
+
+class Base:
+    """
+    The base of all other classes in this project
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
