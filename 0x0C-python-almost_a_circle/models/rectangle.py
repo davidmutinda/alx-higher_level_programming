7c824a4a6647f29f9cb6207df3627b1aa452e965
devmutinda <davidkymutinda@gmail.com>
2022-07-14 16:42:32 +0000

    Added area method to models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,36 +37,72 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        width getter
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        self.check_for_error("width", value, True)
+        """
+        width setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 1:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
+        """
+        height getter
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        self.check_for_error("height", value, True)
+        """
+        height setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 1:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
+        """
+        x getter
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        self.check_for_error("x", value)
+        """
+        x setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        """
+        y getter
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.check_for_error("y", value)
+        """
+        y setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
