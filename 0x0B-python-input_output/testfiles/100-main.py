7afc8339ea3316329f04c31578b3c0b900b1391d
devmutinda <davidkymutinda@gmail.com>
2022-07-15 11:26:55 +0000

    Made changes

--- 0x0B-python-input_output/testfiles/100-main.py
+++ 0x0B-python-input_output/testfiles/100-main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 append_after = __import__('100-append_after').append_after
 
-append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+filename = "file_0"
+text_search = "c"
+text_append = "Python is cool!\n"
+append_after(filename, text_search, text_append)
