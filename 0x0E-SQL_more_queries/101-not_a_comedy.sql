7acfcc20681ca1e43d3a38f13912b89a0d57080b
devmutinda <davidkymutinda@gmail.com>
2022-08-11 19:33:27 +0000

    Create 101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+-- This script lists all shows without the genre "Comedy" in the database "hbtn_0d_tvshows"
+SELECT sh.title
+FROM tv_shows AS sh
+WHERE sh.title NOT IN
+(
+	SELECT sh.title
+	FROM tv_shows AS sh
+	INNER JOIN tv_show_genres AS sg
+	ON sh.id = sg.show_id
+	INNER JOIN tv_genres as ge
+	ON ge.id = sg.genre_id
+	WHERE ge.name = 'Comedy'
+)
+ORDER BY sh.title;
