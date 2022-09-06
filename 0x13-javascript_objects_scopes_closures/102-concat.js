b4a29bbcd3632f7b42f9a4e5a11e3b82f119fe60
devmutinda <davidkymutinda@gmail.com>
2022-09-06 12:24:45 +0000

    Modified file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
+const array = process.argv;
 let data;
-fs.readFile('./fileA', 'utf-8', (err, d) => {
+fs.readFile(`./${array[2]}`, 'utf-8', (err, d) => {
   if (err) {
     console.error(err);
     return;
   }
   data = d;
-  fs.readFile('./fileB', 'utf-8', (err, d) => {
+  fs.readFile(`./${array[3]}`, 'utf-8', (err, d) => {
     if (err) throw err;
     data += d;
-    fs.writeFile('./fileC', data, (err) => {
+    fs.writeFile(`./${array[4]}`, data, (err) => {
       if (err) throw err;
     });
   });
