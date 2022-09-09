f55766f8208e565ffa0c59a5bdc008e730dac086
devmutinda <davidkymutinda@gmail.com>
2022-09-09 18:47:18 +0000

    Fixed circular import

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,16 +2,18 @@
 """
 This module lists all cities from the database hbtn_0e_4_usa
 """
-import MySQLdb
-from sys import argv
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-db = MySQLdb.connect(host='localhost',
-                     user=argv[1],
-                     passwd=argv[2],
-                     db=argv[3])
-cur = db.cursor()
-cur.execute("SELECT cities.id,cities.name,states.name FROM cities\
-            LEFT JOIN states ON cities.state_id = states.id")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    db = MySQLdb.connect(host='localhost',
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id,cities.name,states.name FROM cities\
+                LEFT JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
