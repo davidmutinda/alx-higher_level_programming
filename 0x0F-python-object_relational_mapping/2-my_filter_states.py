b1b0ac5a6ea2ef6a85016eccc8c2ec08d17d1b0e
devmutinda <davidkymutinda@gmail.com>
2022-09-09 17:51:21 +0000

    Created task 2 files

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module lists all states from the database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name =\
+             '{}' ORDER BY id".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
