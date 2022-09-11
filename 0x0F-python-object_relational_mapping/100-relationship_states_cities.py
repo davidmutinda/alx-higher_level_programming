386702b484731aefe9f00cd108ae2975bc0724e3
devmutinda <davidkymutinda@gmail.com>
2022-09-11 12:17:46 +0000

    Created task 15 files

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from relationship_state import State
+from relationship_city import City, Base
+from sqlalchemy.orm import sessionmaker, relationship
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='Carlifonia')
+    session.add(new_state)
+    new_state.cities.append(City(name='San Francisco'))
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains the class definition of a State
+and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from relationship_city import Base
+
+
+class State(Base):
+    """
+    Creates user table
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", cascade="all, delete")
