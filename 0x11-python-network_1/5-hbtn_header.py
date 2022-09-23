025f28a035fd1d4326986c091e0f205032402d11
devmutinda <davidkymutinda@gmail.com>
2022-09-23 19:27:04 +0000

    Modified

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
+    r = requests.get(argv[1])
     print(r.headers.get('X-Request-Id'))
