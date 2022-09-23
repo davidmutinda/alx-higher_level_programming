9e8dbc5862b221753cdbcdc3d586c7e34dc19ba3
devmutinda <davidkymutinda@gmail.com>
2022-09-23 18:13:27 +0000

    Created task 5 file

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL, sends a request to the URL and displays
+the value of the variable "X-Request-Id" in the response header
+"""
+import requests
+from sys import argv
+
+r = requests.get('https://alx-intranet.hbtn.io/status')
+print(dict(r.headers).get('X-Request-Id'))
