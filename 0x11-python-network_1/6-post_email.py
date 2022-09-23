d45b6bb5ba4d88cc70fe7cf91d3014fda27a93b7
devmutinda <davidkymutinda@gmail.com>
2022-09-23 18:20:36 +0000

    Created task 6 file

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+"""
+import requests
+from sys import argv
+
+data = {"email": argv[2]}
+r = requests.post(argv[1], data)
+print(r.content.decode('utf-8'))
