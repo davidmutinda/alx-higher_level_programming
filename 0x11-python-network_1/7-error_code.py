e995278e6b87de20c7f85d6da5675be5e210a29c
devmutinda <davidkymutinda@gmail.com>
2022-09-23 19:09:45 +0000

    Created task 7 file

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL, sends a request to the URL and displays the body
+of the response (decoded in utf-8)
+"""
+from sys import argv
+import response
+
+
+if __name__ == "__main__":
+    try:
+        req = requests.get(argv[1], auth=('user', 'pass'))
+        print(req.content.decode('utf-8'))
+    except Exception as e:
+        print('Error code:', e.code)
