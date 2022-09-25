d3447cf908ae5cc76ba03227204a44755ee8827a
devmutinda <davidkymutinda@gmail.com>
2022-09-25 12:34:06 +0000

    Created task 100 file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script lists 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = requests.get(f'https://api.github.com/repos\
+/{argv[2]}/{argv[1]}/commits')
+    for count, r in enumerate(req.json()):
+        if count == 10:
+            break
+        print(r.get('sha'), r.get('commit').get('author').get('name'))
