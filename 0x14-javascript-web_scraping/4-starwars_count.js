21f155988ad6f21ee5e1492098635ef38a9243b7
devmutinda <davidkymutinda@gmail.com>
2022-10-18 09:38:40 +0000

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,16 @@
 
 const request = require('request');
 const url = process.argv[2];
-const id = 'https://swapi-api.hbtn.io/api/people/18/';
+const id = '18';
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
   let number = 0;
   const episodes = JSON.parse(body).results;
   episodes.forEach(el => {
-    if (el.characters.includes(id)) number++;
+    el.characters.forEach(n => {
+      if (n.includes(id)) number++;
+    });
   });
 
   console.log(number);
