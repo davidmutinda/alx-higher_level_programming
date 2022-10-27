79bc11605e95459c6dc9b1d0ea45764590b02c2b
devmutinda <davidkymutinda@gmail.com>
2022-10-27 15:24:23 +0300

    say hello

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,5 @@
-$(()=> {
-    $.get('https://swapi-api.hbtn.io/api/films/?format=json', data => {
-        $.each(data.results, (index, value) => {
-            $('UL#list_movies').append(`<li>${value.title}</li>`);
-        })
-    });
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', data => {
+    $.each(data.results, (index, value) => {
+        $('UL#list_movies').append(`<li>${value.title}</li>`);
+    })
 });
