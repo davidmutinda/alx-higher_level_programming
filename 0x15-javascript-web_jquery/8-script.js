ea920d6d4815f7c7238aca3d965456cdf84c8ec8
devmutinda <davidkymutinda@gmail.com>
2022-10-27 14:29:20 +0300

    Star wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(()=> {
+    $.get('https://swapi-api.hbtn.io/api/films/?format=json', data => {
+        $.each(data.results, (index, value) => {
+            $('UL#list_movies').append(`<li>${value.title}</li>`);
+        })
+    });
+});
